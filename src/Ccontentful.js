bd808dd4981666b1578b758cb9b203b3f3d1d948
RoyLo <roylo0628@gmail.com>
2019-10-04 14:55:28 +0800

    2019/10/4/1455

--- src/Ccontentful.js
+++ src/Ccontentful.js
@@ -1,6 +1,9 @@
 import { createClient } from 'contentful'
 
 export default createClient({
-  space: process.env.REACT_APP_API_SPACE,
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN
+  space: "hgi5rn93myov",
+  accessToken: "qxxl8hq4MKTyi_1s8p41JDRmpMLTat2ODSfKzJLLLN4"
 });
+
+// REACT_APP_API_SPACE=hgi5rn93myov
+// REACT_APP_ACCESS_TOKEN=qxxl8hq4MKTyi_1s8p41JDRmpMLTat2ODSfKzJLLLN4
